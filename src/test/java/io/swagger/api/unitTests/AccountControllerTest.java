1711c200b895eb65911d0e56c8ef94c4a2e947b2
Liestere <550178@student.inholland.nl>
2019-06-13 20:16:10 +0200

    base

--- src/test/java/io/swagger/api/unitTests/AccountControllerTest.java
+++ src/test/java/io/swagger/api/unitTests/AccountControllerTest.java
@@ -1,14 +1,20 @@
 package io.swagger.api.unitTests;
 
+import io.swagger.api.AccountsApiController;
 import io.swagger.model.Account;
 import io.swagger.model.CurrentAccount;
 import io.swagger.model.Iban;
 import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
+@RunWith(SpringRunner.class)
+@WebMvcTest(AccountsApiController.class)
 public class AccountControllerTest extends TransactionControllerTest {
 
     protected List<Account> accounts = new ArrayList<>();
